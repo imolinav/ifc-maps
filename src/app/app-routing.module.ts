11a111ebe66779ff11c557b5e3bdbcb6174e14af
imolinav <iamovaz@gmail.com>
2022-01-21 12:59:00 +0100

    fix: clean specs files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'home', loadChildren: () => import ('./pages/home/home.module').then(m => m.HomeModule) },
   { path: 'visualizer', loadChildren: () => import('./pages/visualizer/visualizer.module').then(m => m.VisualizerModule) },
-  { path: 'editor', loadChildren: () => import('./pages/editor/editor.module').then(m => m.EditorModule) },
   { path: 'search', loadChildren: () => import('./pages/search/search.module').then(m => m.SearchModule) },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
